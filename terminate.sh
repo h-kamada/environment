a10189ed7acae868e5d26161b0bc1baa533da5df
Kamada Hitoshi <h-kamada@jsk.imi.i.u-tokyo.ac.jp>
2015-07-21 20:18:33 +0900

    modify

--- terminate.sh
+++ terminate.sh
@@ -1,5 +1,11 @@
-sudo aptitude update > /home/h-kamada/terminatelog 2>&1
-sudo aptitude upgrade 2>&1 >> /home/h-kamada/terminatelog
-cd /home/h-kamada/ros/hydro/src 2>&1 >> /home/h-kamada/terminatelog
-wstool update 2>&1 >> /home/h-kamada/terminatelog
-catkin build 2>&1 >> /home/h-kamada/terminatelog
+sudo aptitude update
+sudo aptitude upgrade
+cd /home/h-kamada/ros/hydro/src
+wstool update
+catkin build
+
+# sudo aptitude update > /home/h-kamada/terminatelog 2>&1
+# sudo aptitude upgrade 2>&1 >> /home/h-kamada/terminatelog
+# cd /home/h-kamada/ros/hydro/src 2>&1 >> /home/h-kamada/terminatelog
+# wstool update 2>&1 >> /home/h-kamada/terminatelog
+# catkin build 2>&1 >> /home/h-kamada/terminatelog
