9287a7886550cc2f2b8a87770485bc0c19fbd408
Hitoshi Kamada <doctorteawater@gmail.com>
2015-07-23 10:49:18 +0900

    poweroff

--- poweroff.sh
+++ poweroff.sh
@@ -2,7 +2,7 @@
 echo "executing poweroff. Are you sure? to poweroff? [y/N]"
 read answer
 if [ "$answer" == "yes" -o "$answer" == "y" ]; then
-    echo "poweroff"
+    sudo poweroff
 fi
 
-# sudo poweroff
+
---
+++ poweroffwithupdate.sh
@@ -0,0 +1,20 @@
+echo "executing poweroffwithupdate. Are you sure? to poweroff? [y/N]"
+read answer
+
+flag="false"
+function error {
+    flag="true"
+    echo "interrupt"
+}
+trap error ERR
+
+if [ "$answer" == "yes" -o "$answer" == "y" ]; then
+    . /home/h-kamada/environment/update.sh
+fi
+
+if [ \( "$answer" == "yes" -o "$answer" == "y" \) -a "$flag" == "false" ]; then
+    sudo poweroff
+fi
+
+echo "cancel poweroffwithupdate"
+
