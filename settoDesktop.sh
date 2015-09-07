da29b59c4ca9a54e11dc7fe239b9123509c464e0
Kamada Hitoshi <h-kamada@jsk.imi.i.u-tokyo.ac.jp>
2015-09-07 11:53:28 +0900

    settoDesktop.sh

--- settoDesktop.sh
+++ settoDesktop.sh
@@ -5,15 +5,16 @@ if [ -e "/home/h-kamada/Desktop/poweroff.desktop" ]; then
     echo "rm ~/Desktop/poweroff.desktop"
     rm -rf /home/h-kamada/Desktop/poweroff.desktop
 fi
-if [ -e "/home/h-kamada/Desktop/poweroffwithupdate.desktop" ]; then
-    echo "rm ~/Desktop/poweroffwithupdate.desktop"
-    rm -rf /home/h-kamada/Desktop/poweroffwithupdate.desktop
-fi
+# if [ -e "/home/h-kamada/Desktop/poweroffwithupdate.desktop" ]; then
+#     echo "rm ~/Desktop/poweroffwithupdate.desktop"
+#     rm -rf /home/h-kamada/Desktop/poweroffwithupdate.desktop
+# fi
 if [ -e "/home/h-kamada/Desktop/update.desktop" ]; then
     echo "rm ~/Desktop/update.desktop"
     rm -rf /home/h-kamada/Desktop/update.desktop
 fi
 echo "ln -s /home/h-kamada/environment/poweroff.desktop /home/h-kamada/Desktop/poweroff.desktop"
+
 ln -s $script_dir_path/poweroff.desktop /home/h-kamada/Desktop/poweroff.desktop
 echo "ln -s /home/h-kamada/environment/poweroffwithupdate.desktop /home/h-kamada/Desktop/poweroffwithupdate.desktop"
 ln -s $script_dir_path/poweroffwithupdate.desktop /home/h-kamada/Desktop/poweroffwithupdate.desktop
